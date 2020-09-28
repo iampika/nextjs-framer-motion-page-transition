ce0ab5a4fa6d7545d0c9a36e5e2a38542f967c95
Mahendra <pikaatic@gmail.com>
2020-09-28 18:28:53 +0530

    🔥🔥

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
 import { AnimatePresence, motion } from 'framer-motion'
 import '../styles/tailwind.css'
 
+function Layout({ children }) {
+  return <div className="overflow-hidden">{children}</div>
+}
+
 function MyApp({ Component, pageProps, router }) {
   return (
-    <AnimatePresence initial={false}>
-      <motion.div
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        key={router.route}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <Layout>
+      <AnimatePresence initial={false}>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          key={router.route}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
   )
 }
 
